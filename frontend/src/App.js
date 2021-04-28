bd7a4054e9a54fb710e16b601c5338dba0f32f38
Mr.Imas <imas1997o@gmail.com>
2021-04-28 16:06:21 +0300

    added router to app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import { MainNavigation } from './shared/components/MainNavigation';
 
 const App = () => {
-  return <MainNavigation />;
+  return (
+    <Router>
+      <MainNavigation />
+      <Switch>
+        <Route path='/' exact>
+          <div>hello /</div>
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
