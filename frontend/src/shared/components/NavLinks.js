a59190909067c7b4b38d79f6f8361a647ea379ad
Mr.Imas <imas1997o@gmail.com>
2021-04-29 17:12:47 +0300

    styled a bit the nav links

--- frontend/src/shared/components/NavLinks.js
+++ frontend/src/shared/components/NavLinks.js
@@ -3,31 +3,47 @@ import { NavLink } from 'react-router-dom';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import { IconButton } from '@material-ui/core';
 
+import './NavLinks.css';
+
 export const NavLinks = () => {
   return (
-    <ul>
+    <ul className='nav-links'>
       <li>
         <NavLink to='/' exact>
           PRODUCTS
         </NavLink>
+      </li>
+      <li>
         <NavLink to='/' exact>
           SHOP
         </NavLink>
+      </li>
+      <li>
         <NavLink to='/products/new' exact>
           CREATE PRODUCT
         </NavLink>
+      </li>
+      <li>
         <NavLink to='/category' exact>
           CATEGORIES
         </NavLink>
+      </li>
+      <li>
         <NavLink to='/login' exact>
           LOGIN&REGISTER
         </NavLink>
+      </li>
+      <li>
         <NavLink to='/history' exact>
           HISTORY
         </NavLink>
+      </li>
+      <li>
         <NavLink to='/' exact>
           LOGOUT
         </NavLink>
+      </li>
+      <li>
         <NavLink to='/' exact>
           <IconButton>
             <ShoppingCartIcon />
