cc25f05a8083a9662ce34b0690aacd516f2c14aa
Mr.Imas <imas1997o@gmail.com>
2021-05-20 14:03:58 +0300

    added http error model and updated the invalid routing middleware

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 
 import productsRouter from './routes/products-routes.js';
+import HttpError from './models/http-errors.js';
 
 const app = express();
 
@@ -10,7 +11,7 @@ app.use(bodyParser.json());
 app.use('/api/products', productsRouter);
 
 app.use((req, res, next) => {
-  res.status(404).json({ message: 'Could not find this route' });
+  next(new HttpError('Could not find this route', 404));
 });
 
 app.listen(5000, () => console.log('server is listeining on port 5000'));
