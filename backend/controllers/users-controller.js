cb517d17ef0ca160ce1070242f85e7a8bc2e8801
Mr.Imas <imas1997o@gmail.com>
2021-05-20 16:37:44 +0300

    added basic login to users controller in backend

--- backend/controllers/users-controller.js
+++ backend/controllers/users-controller.js
@@ -25,3 +25,17 @@ export const signup = (req, res, next) => {
   DUMMY_USERS.push(newUser);
   res.status(201).json({ user: newUser });
 };
+
+export const login = (req, res, next) => {
+  const { email, password } = req.body;
+  const hasUser = DUMMY_USERS.find((u) => u.email === email);
+  if (!hasUser || hasUser.password !== password) {
+    return next(
+      new HttpError(
+        'Could not identify user, credentials seem to be wrong',
+        401
+      )
+    );
+  }
+  res.json({ message: 'user logged in' });
+};
--- backend/routes/users-routes.js
+++ backend/routes/users-routes.js
@@ -4,9 +4,7 @@ import * as usersController from '../controllers/users-controller.js';
 
 const usersRouter = express.Router();
 
-usersRouter.post('/login', (req, res, next) =>
-  res.json({ message: 'logged in' })
-);
+usersRouter.post('/login', usersController.login);
 
 usersRouter.post('/signup', usersController.signup);
 
