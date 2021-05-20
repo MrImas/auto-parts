514aa1e428b30466c5dbe67b4cc9e0da910e852e
Mr.Imas <imas1997o@gmail.com>
2021-05-20 14:00:27 +0300

    added 404 message for invalid routing

--- backend/server.js
+++ backend/server.js
@@ -9,4 +9,8 @@ app.use(bodyParser.json());
 
 app.use('/api/products', productsRouter);
 
+app.use((req, res, next) => {
+  res.status(404).json({ message: 'Could not find this route' });
+});
+
 app.listen(5000, () => console.log('server is listeining on port 5000'));
