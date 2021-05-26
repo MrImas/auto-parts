9053c88f3e0efda51d82bee571d0e1cf92daef35
Mr.Imas <imas1997o@gmail.com>
2021-05-26 15:01:53 +0300

    created error modal without checking if it works yet

---
+++ frontend/src/shared/components/UIElements/ErrorModal.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Modal from './Modal';
+
+export const ErrorModal = (props) => {
+  return (
+    <Modal
+      open={Boolean(props.error)}
+      onClose={props.clearError}
+      title='Error Has Occured... :('
+    >
+      {props.error}
+    </Modal>
+  );
+};
