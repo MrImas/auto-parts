5d584955d090bfa64e0ff5218d33eb498912f329
Mr.Imas <imas1997o@gmail.com>
2021-05-26 19:13:13 +0300

    fetch categories from db and display in the frontend works

--- frontend/src/categories/pages/Categories.js
+++ frontend/src/categories/pages/Categories.js
@@ -1,23 +1,15 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { ErrorModal } from '../../shared/components/UIElements/ErrorModal';
+import { LoadingSpinner } from '../../shared/components/UIElements/LoadingSpinner';
 
+import { useHttpClient } from '../../shared/hooks/http-hook';
 import { AddCategory } from '../components/AddCategory';
 import { CategoryList } from '../components/CategoryList';
 import './Categories.css';
 
-const DUMMY_CATEGORIES = [
-  {
-    name: 'category 1',
-  },
-  {
-    name: 'category 2',
-  },
-  {
-    name: 'category 3',
-  },
-];
-
 export const Categories = () => {
-  const [categories, setCategories] = useState(DUMMY_CATEGORIES);
+  const [isLoading, error, sendHttpRequest, clearError] = useHttpClient();
+  const [categories, setCategories] = useState();
 
   const addCategoryHandler = (categoryName) => {
     setCategories([...categories, { name: categoryName.trim() }]);
@@ -29,10 +21,28 @@ export const Categories = () => {
     );
   };
 
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const responseData = await sendHttpRequest(
+          'http://localhost:5000/api/categories'
+        );
+        setCategories(responseData.categories);
+      } catch (err) {}
+    };
+    fetchCategories();
+  }, [sendHttpRequest]);
+
   return (
-    <div className='categories'>
-      <AddCategory categories={categories} onAdd={addCategoryHandler} />
-      <CategoryList items={categories} onDelete={deleteCategoryHandler} />
-    </div>
+    <>
+      {error && <ErrorModal error={error} clearError={clearError} />}
+      {isLoading && <LoadingSpinner />}
+      {!isLoading && categories && (
+        <div className='categories'>
+          <AddCategory categories={categories} onAdd={addCategoryHandler} />
+          <CategoryList items={categories} onDelete={deleteCategoryHandler} />
+        </div>
+      )}
+    </>
   );
 };
